ca818928b0126754dee14ce4b18a0fc9d2f2df0b
Lorenzo Volpi <lorenzo.volpi@studenti.unimi.it>
2021-10-28 13:06:30 +0200

    versione 0.02

--- prova.git/src/prova/git/es.java
+++ prova.git/src/prova/git/es.java
@@ -5,6 +5,7 @@ public class es {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("asdasdwasdwasdadssssss");
+		System.out.println("modifica");
 	}
 
 }
