b321388bc304c4b13850dfb30f434a9267de8caf
Lorenzo Volpi <lorenzo.volpi@studenti.unimi.it>
2021-10-29 11:17:50 +0200

    versione 0.01

--- IEVS/src/main/java/Elettore.java
+++ IEVS/src/main/java/Elettore.java
@@ -1,4 +1,8 @@
 
 public class Elettore {
-
+	private String nome;
+	private String cognome;
+	private String cdi;
+	private String data_di_nascita;
+	private String tessera_elettorale;
 }
---
+++ IEVS/src/main/java/Gestore.java
@@ -0,0 +1,7 @@
+
+public class Gestore {
+	private String nome;
+	private String cognome;
+	private String matricola;
+	private String ruolo;
+}
