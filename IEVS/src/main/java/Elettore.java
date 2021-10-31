140b66b255edb1631278b3e2acba5aba99d06bd3
lollo <lollo@DESKTOP-SB54C1K>
2021-10-31 22:22:26 +0100

    Versione 0.03:
    -Aggiunti costruttori e metodi a classi Elettore e Scrutatore
    -Aggiunte prime specifiche ai metodi

--- IEVS/src/main/java/Elettore.java
+++ IEVS/src/main/java/Elettore.java
@@ -5,4 +5,68 @@ public class Elettore {
 	private String cdi;
 	private String data_di_nascita;
 	private String tessera_elettorale;
+	
+	//REQUIRES: Tutti gli argomenti != null
+	//EFFECTS: Costruisce un nuovo oggetto Elettore con attributi uguali agli argomenti
+	public Elettore(String n, String c,String d,String ddn,String t) {
+		nome=n;
+		cognome=c;
+		cdi=d;
+		data_di_nascita=ddn;
+		tessera_elettorale=t;
+	}
+	
+	//EFFECTS: Restituisce il numero di carta d'identità
+	public String getCdi() {
+		return cdi;
+	}
+	
+	//EFFECTS: Restituisce il cognome
+	public String getCognome() {
+		return cognome;
+	}
+	
+	//EFFECTS: Restituisce la data di nascita
+	public String getData_di_nascita() {
+		return data_di_nascita;
+	}
+	
+	//EFFECTS: Restituisce il nome
+	public String getNome() {
+		return nome;
+	}
+	
+	//EFFECTS: Restituisce il numero di tessera elettorale
+	public String getTessera_elettorale() {
+		return tessera_elettorale;
+	}
+	
+	//EFFECTS: Imposta il numero di tessera elettorale
+	//MODIFIES: Modifica il numero di tessera elettorale
+	public void setCdi(String cdi) {
+		this.cdi = cdi;
+	}
+	
+	//EFFECTS: Imposta il cognome
+	//MODIFIES: Modifica il cognome
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	//EFFECTS: Imposta la data di nascita
+	//MODIFIES: Modifica la data di nascita
+	public void setData_di_nascita(String data_di_nascita) {
+		this.data_di_nascita = data_di_nascita;
+	}
+	
+	//EFFECTS: Imposta il nome
+	//MODIFIES: Modifica il nome
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//MODIFIES: Modifica il numero di tessera elettorale
+	public void setTessera_elettorale(String tessera_elettorale) {
+		this.tessera_elettorale = tessera_elettorale;
+	}
 }
--- IEVS/src/main/java/Scrutatore.java
+++ IEVS/src/main/java/Scrutatore.java
@@ -4,4 +4,58 @@ public class Scrutatore {
 	private String cognome;
 	private String matricola;
 	private String cdi;
+	
+	//REQUIRES: Tutti gli argomenti != null
+	//EFFECTS: Costruisce un nuovo oggetto Scrutatore con attributi uguali agli argomenti
+	public Scrutatore(String n,String c,String m, String d) {	
+		nome=n;
+		cognome=c;
+		matricola=m;
+		cdi=d;
+	}
+	
+	//EFFECTS: Restituisce il numero di carta d'identità
+	public String getCdi() {
+		return cdi;
+	}
+
+	//EFFECTS: Restituisce il nome
+	public String getNome() {
+		return nome;
+	}
+
+	//EFFECTS: Restituisce il nome
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	//EFFECTS: Restituisce il cognome
+	public String getCognome() {
+		return cognome;
+	}
+
+	//EFFECTS: Imposta il cognome
+	//MODIFIES: Modifica il cognome
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	//EFFECTS: Restituisce la matricola
+	public String getMatricola() {
+		return matricola;
+	}
+
+	//EFFECTS: Imposta la matricola
+	//MODIFIES: Modifica la matricola
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+
+	//EFFECTS: Imposta il numero di carta d'identità
+	//MODIFIES: Modifica il numero di carta d'identità
+	public void setCdi(String cdi) {
+		this.cdi = cdi;
+	}
+	
+	
 }
